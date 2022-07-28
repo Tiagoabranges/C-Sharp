39d4900032686a59f1d69c60f2b7e875ccc7ec0f
Tiago Abranges <tiagoabranges@gmail.com>
2022-07-28 12:52:18 -0300

    build realizado

---
+++ Tipos/Program.cs
@@ -0,0 +1,17 @@
+﻿//Declarando uma variável do tipo string ideal para dados literários e frases 
+string excerptFromBook = "Ser ou não ser, eis a questão";
+
+//Declarando uma variável float que pode armazenar um valor de ponto flutuante com precisão de até 7 digitos
+// O sufixo f informa o compilador que o valor é float
+float floatValue = 100.0f;
+
+//Declarando uma variável double que pode armazenar um valor de ponto flutuante com precisão de até 15 digitos
+// O sufixo d informa o compilador que o valor é um double
+double doubleValue = 100.0d;
+
+//Declarando uma variável decimal que pode armazenar um valor de ponto flutuante com precisão de até 29 digitos.
+// O sufixo M informa o compilador que o valor é um decimal
+decimal decimalValue = 100.0M;
+
+//Declarando uma variável char que pode armazenar um unico caractere.
+char letter = 'T'; 
