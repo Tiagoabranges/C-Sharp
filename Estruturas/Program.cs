487accebdf7be6604315fcfae65d4d7703f758f1
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-05 18:08:27 -0300

    :sparkles:if

--- Estruturas/Program.cs
+++ Estruturas/Program.cs
@@ -1,23 +1,5 @@
-﻿public class Program
+﻿if (condição)
 {
-    public static void Main()
-    {
-        Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
-        string? response = Console.ReadLine();
-        int radius = 0;
-        var canConvert = Int32.TryParse(response, out radius);
-
-        if(canConvert)
-        {
-            const double pi = 3.14159;
-
-            double circumference = pi * (2 * radius);
-
-            Console.WriteLine("A circunferência de um circulo com raio "+ radius + " é igual a " + circumference);
-        }
-        else
-        {
-            Console.WriteLine("O texto digitado não é um número inteiro.");
-        }
-    }            
+    //bloco de código a ser executado quando a condição for VERDADEIRA    
+    comandos;
 }
