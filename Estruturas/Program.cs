989bdee487dcb840a48a76687b87af12f8367bb7
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-05 18:17:54 -0300

    :sparkles: triangulo funcao

--- Estruturas/Program.cs
+++ Estruturas/Program.cs
@@ -1,12 +1,33 @@
-﻿switch (number)
-{                
-    case > 0:
-        Console.WriteLine("maior que 0");
-        break;               
-    case 0:
-        Console.WriteLine("igual a zero");
-        break;
-    default:                    
-        Console.WriteLine("menor que zero");
-        break;
+﻿public static string IdentifyTriangle(double xSide, double ySide, double zSide)
+{       
+    var name = "";
+
+
+    if(xSide == ySide && xSide == zSide)
+    {
+        name = "Triângulo Equilátero";
+    }
+    else if((xSide == ladoB) || (xSide == zSide) || (ySide ==zSide))
+    {
+        name = "Triângulo Isóscele";
+    }
+    else
+    {
+        name = "Triângulo Escaleno";
+    }
+    return name;
 }
+
+
+    [Theory(DisplayName = "Deve classificar um triangulo")]
+    [InlineData(2,2,2, "Triângulo Equilátero")]
+    [InlineData(5,6,6, "Triângulo Isóscele")]
+    [InlineData(6,2,6, "Triângulo Isóscele")]
+    [InlineData(6,6,2, "Triângulo Isóscele")]
+    [InlineData(1,2,3, "Triângulo Escaleno")]
+    public void TestIdentifyTriangle(double xSide, double ySide, double zSide, string name)
+    {
+        var resultName = Triangle.IdentifyTriangle(xSide, ySide, zSide);
+
+        resultName.Should().Be(name);
+    }
