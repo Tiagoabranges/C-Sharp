b4d10073704895c02583e3e8b645f32a494f35ee
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-05 18:19:38 -0300

    :sparkles: poligono

--- Estruturas/Program.cs
+++ Estruturas/Program.cs
@@ -1,33 +1,23 @@
-﻿public static string IdentifyTriangle(double xSide, double ySide, double zSide)
-{       
-    var name = "";
-
-
-    if(xSide == ySide && xSide == zSide)
+﻿public static string IdentifyPolygon(int sideCount)
+{
+    var name = string.Empty;
+    switch (sideCount)
     {
-        name = "Triângulo Equilátero";
-    }
-    else if((xSide == ladoB) || (xSide == zSide) || (ySide ==zSide))
-    {
-        name = "Triângulo Isóscele";
-    }
-    else
-    {
-        name = "Triângulo Escaleno";
+        case < 3:
+            name = "Não é um polígono";
+            break;
+        case 3:
+            name = "Triângulo";
+            break;
+        case 4:
+            name = "Quadrado";
+            break;
+        case 5:
+            name = "Pentágono";
+            break;
+        default:
+            name = "Polígono não identificado";
+            break;
     }
     return name;
 }
-
-
-    [Theory(DisplayName = "Deve classificar um triangulo")]
-    [InlineData(2,2,2, "Triângulo Equilátero")]
-    [InlineData(5,6,6, "Triângulo Isóscele")]
-    [InlineData(6,2,6, "Triângulo Isóscele")]
-    [InlineData(6,6,2, "Triângulo Isóscele")]
-    [InlineData(1,2,3, "Triângulo Escaleno")]
-    public void TestIdentifyTriangle(double xSide, double ySide, double zSide, string name)
-    {
-        var resultName = Triangle.IdentifyTriangle(xSide, ySide, zSide);
-
-        resultName.Should().Be(name);
-    }
