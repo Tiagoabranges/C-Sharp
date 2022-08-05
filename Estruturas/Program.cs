a2f6aae48bb33da0682161ce39d46572b67bbbe6
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-05 18:19:54 -0300

    :sparkles: poligono tests

--- Estruturas/Program.cs
+++ Estruturas/Program.cs
@@ -21,3 +21,16 @@
     }
     return name;
 }
+
+    [Theory(DisplayName = "Deve classificar um polígono.")]
+    [InlineData(2,"Não é um polígono")]
+    [InlineData(3,"Triângulo")]
+    [InlineData(4,"Quadrado")]
+    [InlineData(5,"Pentágono")]
+    [InlineData(6,"Polígono não identificado")]
+    public void TestIdentifyPolygon(int sideCount, string name)
+    {
+        var resultName = Polygon.ClassificarPoligono(name);
+
+        resultName.Should().Be(name);
+    }
