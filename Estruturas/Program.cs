12e2be3b0409cb342336c1f9b6c0731d135ba766
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-05 18:08:42 -0300

    :sparkles:if

--- Estruturas/Program.cs
+++ Estruturas/Program.cs
@@ -1,5 +1,4 @@
-﻿if (condição)
-{
-    //bloco de código a ser executado quando a condição for VERDADEIRA    
-    comandos;
+﻿if(age > 15)
+{        
+    Console.WriteLine("idade maior que quinze");
 }
