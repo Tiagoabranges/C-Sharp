2b121f821c9307b21491734cc0af3dd2ecd5ceb1
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-09 16:00:58 -0300

    :sparkles: forEach

--- Repeticao/Program.cs
+++ Repeticao/Program.cs
@@ -1,8 +1,10 @@
-﻿for (int count = 0; count < 3; count++)
+﻿// Na declaração do foreach, temos a inicialização de uma variável do mesmo tipo da coleção, 
+// o operador in, a coleção que o laço percorrerá e o bloco de instruções a ser executado. 
+// A cada incremento do laço, um elemento da coleção será atribuído ao elemento inicalizado na 
+// declaração e então o bloco de instruções será executado.
+
+string[] names = new string[] { "Hulk", "Thor", "Loki" };
+foreach (var name in names)
 {
-    Console.WriteLine("numero: " + count);
-}
-for (inicialização; condição; incremento)
-{
-    //comandos;
+    Console.WriteLine(name);
 }
